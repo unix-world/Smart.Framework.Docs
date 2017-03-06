15958fc6d4f5b332d7e2897efc9d0f1f284b0acc
unix-world <iradu@unix-world.org>
2017-03-06 17:32:14 +0200

    TMP svn.46

---
+++ jsdoc/api.js
@@ -0,0 +1,42 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "SmartJS_Archiver_LZS",
+        "SmartJS_Base64",
+        "SmartJS_BrowserUtils",
+        "SmartJS_CoreUtils",
+        "SmartJS_CryptoBlowfish",
+        "SmartJS_CryptoHash",
+        "Test_Browser_Compliance"
+    ],
+    "modules": [
+        "Smart.Framework_JS_Archivers",
+        "Smart.Framework_JS_Browser",
+        "Smart.Framework_JS_Core",
+        "Smart.Framework_JS_Crypto"
+    ],
+    "allModules": [
+        {
+            "displayName": "Smart.Framework/JS/Archivers",
+            "name": "Smart.Framework_JS_Archivers",
+            "description": "CLASS :: LZS Archiver / Unarchiver"
+        },
+        {
+            "displayName": "Smart.Framework/JS/Browser",
+            "name": "Smart.Framework_JS_Browser",
+            "description": "CLASS :: Browser Test Compliance"
+        },
+        {
+            "displayName": "Smart.Framework/JS/Core",
+            "name": "Smart.Framework_JS_Core",
+            "description": "CLASS :: Core Utils"
+        },
+        {
+            "displayName": "Smart.Framework/JS/Crypto",
+            "name": "Smart.Framework_JS_Crypto",
+            "description": "CLASS :: Base64 Encode / Decode"
+        }
+    ],
+    "elements": []
+} };
+});
